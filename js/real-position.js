73664bdbfa739ab94cf38621035b021cfbff5846
IvyWooo <deewooo299@gmail.com>
2023-08-06 16:01:34 +0800

    feat:排序

--- js/real-position.js
+++ js/real-position.js
@@ -1,4 +1,4 @@
-import { getValidPositions } from "./service/db.js";
+import { getValidPositions, updatePosition } from "./service/db.js";
 import { today } from "./service/tools.js";
 
 import { getRealQuote } from "./service/quote.js";
@@ -12,19 +12,62 @@ setInterval(function () {
 }, 1000);
 
 export function refreshRealPositonList() {
-  // function refreshRealPositonList() {
-  getValidPositions().then(async function (positions) {
-    const positonListElement = document.getElementById("real-position");
+  getValidPositions()
+    .then(async (positions) => {
+      // 创建一个新的 Promise 列表
+      const promises = positions.map(async (position) => {
+        const code = position.code;
 
-    // 清空列表
-    positonListElement.innerHTML = "";
+        const realQuote = await getRealQuote(code);
+        const number = parseFloat(position.number);
+        const realPrice = realQuote.realPrice;
+        const buyInPrice = parseFloat(position.buy_in_price);
+        const profitLoss = (realPrice - buyInPrice) * number;
 
-    for (let position of positions) {
-      const listItem = await createTr(position);
+        const profitLossRate = (realPrice - buyInPrice) / buyInPrice;
 
-      positonListElement.appendChild(listItem);
-    }
-  });
+        // 如果你希望结果保留6位小数，可以使用 toFixed 方法
+        const profitLossRateRounded = profitLossRate.toFixed(6);
+
+        const profit10 = parseFloat(position.buy_in_price * 1.1).toFixed(6);
+        const profit20 = parseFloat(position.buy_in_price * 1.2).toFixed(6);
+
+        // 为每个位置对象添加新字段
+        return {
+          ...position, // 拷贝原始对象的所有属性
+          currentPrice: realPrice, // 添加 "currentPrice" 字段
+          profitLossRate: profitLossRateRounded, // 添加 "profitLossRate" 字段
+          profitLoss: profitLoss,
+          profit10: profit10,
+          profit20: profit20,
+          // 这里可以添加更多字段...
+        };
+      });
+
+      // 使用 Promise.all 等待所有 Promise 完成，并返回新的位置列表
+      return Promise.all(promises);
+    })
+    .then((updatedPositions) => {
+      // 排序
+      updatedPositions.sort((a, b) => b.profitLossRate - a.profitLossRate);
+
+      console.log(updatedPositions);
+
+      const positonListElement = document.getElementById("real-position");
+
+      // 清空列表
+      positonListElement.innerHTML = "";
+
+      for (let position of updatedPositions) {
+        console.log(position);
+        const listItem = createTr(position);
+        positonListElement.appendChild(listItem);
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      // ...在这里处理错误...
+    });
 }
 
 const deletePosition = function (id) {
@@ -34,60 +77,61 @@ const deletePosition = function (id) {
   }).then(refreshRealPositonList);
 };
 
-async function createTr(position) {
+function createTr(position) {
   const listItem = document.createElement("tr");
 
+  // 写html
   // code
-  const code = `${position.code}`;
   let item = document.createElement("td");
-  item.textContent = code;
+  item.textContent = position.code;
   listItem.appendChild(item);
 
   // name
   item = document.createElement("td");
-  item.textContent = `${position.name}`;
+  item.textContent = position.name;
   listItem.appendChild(item);
 
   // 盈亏
   item = document.createElement("td");
-  // item.textContent = `${position.name}`;
+  item.textContent = position.profitLoss;
+  updateStyle(item, position.profitLoss);
   listItem.appendChild(item);
 
   // 盈亏比
   item = document.createElement("td");
-  // item.textContent = `${position.name}`;
+  item.textContent = position.profitLossRate;
+  updateStyle(item, position.profitLoss);
   listItem.appendChild(item);
 
   // 数量
   item = document.createElement("td");
-  item.textContent = `${position.number}`;
+  item.textContent = position.number;
   listItem.appendChild(item);
 
   // 当前价格
   item = document.createElement("td");
-  const realQuote = await getRealQuote(code);
-  // console.log(realQuote);
-  item.textContent = realQuote.realPrice;
+  item.textContent = position.currentPrice;
+  item.style.color = "orange"; // 将字体颜色设置为橙色
   listItem.appendChild(item);
 
   // 买入价格
   item = document.createElement("td");
-  item.textContent = `${position.buy_in_price}`;
+  item.textContent = position.buy_in_price;
   listItem.appendChild(item);
 
   // 盈10%
   item = document.createElement("td");
-  item.textContent = `${position.buy_in_price * 1.1}`;
+  item.textContent = position.profit10;
   listItem.appendChild(item);
 
   // 盈20%
   item = document.createElement("td");
-  item.textContent = `${position.buy_in_price * 1.2}`;
+  item.textContent = position.profit20;
   listItem.appendChild(item);
 
   // 买入日期
   item = document.createElement("td");
-  item.textContent = `${position.buy_in_date}`;
+  item.textContent = position.buy_in_date;
   listItem.appendChild(item);
 
   // 平仓按钮
@@ -97,7 +141,7 @@ async function createTr(position) {
   button.textContent = "平仓"; // 设置按钮的文本
   button.onclick = function () {
     // 设置按钮的点击事件处理函数
-    deletePosition(parseInt(`${position.id}`));
+    deletePosition(parseInt(position.id));
   };
 
   item.appendChild(button);
@@ -107,3 +151,16 @@ async function createTr(position) {
 }
 
 window.onload = refreshRealPositonList;
+
+function updateStyle(item, profitLoss) {
+  if (profitLoss > 0) {
+    item.style.color = "red"; // 将字体颜色设置为红色
+    item.style.fontWeight = "bold"; // 将字体设置为粗体
+  } else if (profitLoss < 0) {
+    item.style.color = "green"; // 将字体颜色设置为绿色
+    item.style.fontWeight = "bold"; // 将字体设置为粗体
+  } else {
+    item.style.color = "black"; // 将字体颜色设置为黑色
+    item.style.fontWeight = "normal"; // 将字体设置为正常
+  }
+}
